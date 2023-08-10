fe79c085305d1aef09d5a8fbaa861966a20cc850
MaxKay <makhmudov.kay@gmail.com>
2023-08-10 22:52:40 +0500

    .

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { LogoComponent } from 'src/app/shared/svg/logo/logo.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less'],
   standalone: true,
+  imports: [LogoComponent],
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
---
+++ src/app/shared/svg/logo/logo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'svg-logo',
+  templateUrl: './logo.component.svg',
+  standalone: true,
+})
+export class LogoComponent {}
