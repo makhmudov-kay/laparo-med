8f56ce0c32c791d6717939fbdec030887a28d2c3
Kayrat <makhmudov.kay@gmail.com>
2023-10-05 20:00:42 +0500

    .

---
+++ src/app/pages/products/analytic/analytic.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytic',
+  templateUrl: './analytic.component.html',
+  styleUrls: ['./analytic.component.less'],
+  standalone: true,
+})
+export class AnalyticComponent {}
---
+++ src/app/pages/products/apex-pro/apex-pro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apex-pro',
+  templateUrl: './apex-pro.component.html',
+  styleUrls: ['./apex-pro.component.less'],
+  standalone: true,
+})
+export class ApexProComponent {}
