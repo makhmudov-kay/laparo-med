652fe8b034d49c3891020b8300e167eb0344c045
utamuratov <43562558+utamuratov@users.noreply.github.com>
2023-10-06 18:18:52 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -15,84 +15,9 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgxsModule } from '@ngxs/store';
 import { DataState } from './app/shared/store/data/data.state';
 
-<<<<<<< HEAD
-export const ROUTES: Route[] = [
-  {
-    path: '',
-    pathMatch: 'full',
-    loadComponent: () =>
-      import('./app/pages/home/home.component').then((m) => m.HomeComponent),
-  },
-  {
-    path: 'about-us',
-    loadComponent: () =>
-      import('./app/pages/about-us/about-us.component').then(
-        (m) => m.AboutUsComponent
-      ),
-  },
-  {
-    path: 'contacts',
-    loadComponent: () =>
-      import('./app/pages/contacts/contacts.component').then(
-        (m) => m.ContactsComponent
-      ),
-  },
-  {
-    path: 'blog',
-    loadComponent: () =>
-      import('./app/pages/blog/blog.component').then((m) => m.BlogComponent),
-  },
-  {
-    path: 'blog/:id',
-    loadComponent: () =>
-      import('./app/pages/blog-detail/blog-detail.component').then(
-        (m) => m.BlogDetailComponent
-      ),
-  },
-  {
-    path: 'products',
-    loadComponent: () =>
-      import('./app/pages/products/products.component').then(
-        (m) => m.ProductsComponent
-      ),
-    children: [
-      { path: '', pathMatch: 'full', redirectTo: 'list' },
-      {
-        path: 'list',
-        loadComponent: () =>
-          import(
-            './app/pages/products/product-list/product-list.component'
-          ).then((m) => m.ProductListComponent),
-      },
-      {
-        path: 'individuals',
-        loadComponent: () =>
-          import('./app/pages/products/individuals/individuals.component').then(
-            (m) => m.IndividualsComponent
-          ),
-      },
-      {
-        path: 'professionals',
-        loadComponent: () =>
-          import(
-            './app/pages/products/professionals/professionals.component'
-          ).then((m) => m.ProfessionalsComponent),
-      },
-      {
-        path: 'apex-pro',
-        loadComponent: () =>
-          import('./app/pages/products/apex-pro/apex-pro.component').then(
-            (m) => m.ApexProComponent
-          ),
-      },
-    ],
-  },
-];
-=======
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
->>>>>>> aaf4f06187ef6c846f16016daff64ccfe9d9c825
 
 bootstrapApplication(AppComponent, {
   providers: [
