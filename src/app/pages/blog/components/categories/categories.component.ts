6c72b64fdaef9747b061cda90bf566297a33e68e
Kayrat <makhmudov.kay@gmail.com>
2023-11-10 09:06:45 +0500

    .

--- src/app/pages/blog/components/categories/categories.component.ts
+++ src/app/pages/blog/components/categories/categories.component.ts
@@ -1,7 +1,13 @@
 import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { Category } from 'src/app/shared/models/category.model';
 import { MyTranslatePipe } from 'src/app/shared/pipes/my-translate.pipe';
@@ -52,19 +58,36 @@ export class CategoriesComponent {
   /**
    *
    */
+  categoryId!: number;
+
+  /**
+   *
+   */
   value = '';
 
   /**
    *
    * @param router
    */
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private cd: ChangeDetectorRef
+  ) {
+    this.route.queryParams.subscribe((p) => {
+      console.log(p);
+
+      this.categoryId = +p['category_id'];
+      this.cd.markForCheck();
+    });
+  }
 
   /**
    *
    * @param categoryId
    */
   selectCategory(categoryId: number) {
+    this.categoryId = categoryId;
     this.router.navigate([], { queryParams: { category_id: categoryId } });
   }
 
